64c47f6e5398e0f1429b2d657bd00884636219a7
Warner <sxlfybb@gmail.com>
2024-12-14 01:26:43 +0800

    1、增加了高分榜单 2、优化了结束逻辑

---
+++ a2048/HighScores.swift
@@ -0,0 +1,18 @@
+//
+//  HighScores.swift
+//  a2048
+//
+//  Created by Warner Kuo on 2024/12/14.
+//
+
+import SwiftUI
+
+struct HighScores: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HighScores()
+}
