bace809b80276d6a48244b53aafe3813cc50f64a
Warner <sxlfybb@gmail.com>
2024-12-15 23:03:57 +0800

    1、 高分榜美化 2、 游戏结束时逻辑优化 3、对大于3位数字的卡片字体调整优化 4、 高分榜结构体优化

--- a2048/HighScores.swift
+++ a2048/HighScores.swift
@@ -10,14 +10,51 @@ import SwiftUI
 
 struct HighScores: View {
     @ObservedObject var UserData: MainData = MainData()
+//    @State private var sortOrder = [KeyPathComparator(\HighScore.score)]
     
     var body: some View {
-        List {
-            ForEach(UserData.HighScores, id: \.self) { item in
-                Text(String(item))
+
+        Text("Ranking")
+            .font(.system(size: 30))
+            .fontWeight(.bold)
+            .padding()
+        HStack{
+            VStack(alignment: .leading) {
+                HStack{
+                    Text("Rank").bold()
+                        .frame(maxWidth: .infinity)
+                    Text("Score").bold()
+                        .frame(maxWidth: .infinity)
+                    Text("Name").bold()
+                        .frame(maxWidth: .infinity)
+                }
+                ForEach(UserData.HighScores.indices, id:\.self) { index in
+                    HStack{
+                        Text("\(index+1)")
+                            .frame(maxWidth: .infinity)
+                        Text("\(UserData.HighScores[index].score)")
+                            .frame(maxWidth: .infinity)
+                        
+                        Text("\(UserData.HighScores[index].name)")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .padding()
+                    .foregroundStyle(index % 2 == 0 ? Color.white : Color.black)
+                    .background(index % 2 == 0 ? Color(red:0.6, green:0.6, blue:0.8) : Color.clear)
+                }
+                
             }
+            .frame(maxWidth: .infinity)
         }
-        .navigationTitle("High Scores")
+        .padding()
+        Spacer()
+//
+//        Table(UserData.HighScores, sortOrder: $sortOrder) {
+//            TableColumn("Name", value: \.name)
+//            TableColumn("Score", value: \.score) { data in
+//                Text("\(data.score)")
+//            }
+//        }
     }
 }
 
